9132e897679d8c9064da06e594930a8dd839969e
mstosic99 <mihajlostosic99@gmail.com>
2020-12-30 14:21:56 +0100

    generator skoro sve

--- app/Generator.py
+++ app/Generator.py
@@ -1,3 +1,4 @@
+from app.Node import LocalVars, ProcImpl
 from app.Visitor import Visitor
 import re
 
@@ -19,8 +20,15 @@ class Generator(Visitor):
             self.append('\t')
 
     def visit_Program(self, parent, node):
+
         for n in node.nodes:
-            self.visit(node, n)
+            if type(n) is LocalVars:
+                self.visit(node, n)
+
+        for n in node.nodes:
+            if type(n) is not LocalVars:
+                self.visit(node, n)
+
         # self.append('if __name__ == "__main__":')
         # self.newline()
         # self.level += 1
@@ -35,15 +43,16 @@ class Generator(Visitor):
         self.append(';')
 
     def visit_ArrayDecl(self, parent, node):
+        self.visit(node, node.type_)
         self.visit(node, node.id_)
+        self.append('[')
+        self.append(node.to_.value - node.from_.value + 1)
+        self.append(']')
         if node.elems is not None:
-            self.append(' = [')
+            self.append(' =  {')
             self.visit(node, node.elems)
-            self.append(']')
-        elif node.size is not None:
-            self.append(' = ')
-            self.visit(node, node.size)
-            self.append(' * [None]')
+            self.append('}')
+        self.append(';')
 
     def visit_StringDecl(self, parent, node):
         self.visit(node, node.type_)
@@ -63,47 +72,95 @@ class Generator(Visitor):
         self.visit(node, node.id_)
         self.append(' = ')
         self.visit(node, node.expr)
+        self.append(';')
 
     def visit_If(self, parent, node):
-        self.append('if ')
+        self.append('if (')
         self.visit(node, node.cond)
-        self.newline()
-        self.visit(node, node.true)
-        if node.false is not None:
-            self.indent()
-            self.append('else')
-            self.newline()
-            self.visit(node, node.false)
-
-    def visit_While(self, parent, node):
-        self.append('while ')
-        self.visit(node, node.cond)
-        self.append(':')
-        self.newline()
-        self.visit(node, node.block)
-
-    def visit_For(self, parent, node):
-        self.visit(node, node.init)
+        self.append(') {')
         self.newline()
         self.indent()
-        self.append('while ')
+        self.visit(node, node.true)
+        self.newline()
+        self.append('}')
+        self.newline()
+        if node.elseifs is not None:
+            for elseif in node.elseifs:
+                self.visit(node, elseif)
+        if node.false is not None:
+            self.visit(node, node.false)
+
+    def visit_ElseIf(self, parent, node):
+        self.indent()
+        self.append('else if (')
         self.visit(node, node.cond)
-        self.append(':')
+        self.append(')  {')
+        self.visit(node, node.true)
+        self.newline()
+        self.append('}')
+        self.newline()
+
+    def visit_Else(self, parent, node):
+        self.indent()
+        self.append('else {')
+        self.visit(node, node.block)
+        self.newline()
+        self.append('}')
+        self.newline()
+
+    def visit_While(self, parent, node):
+        self.append('while (')
+        self.visit(node, node.cond)
+        self.append(') {')
         self.newline()
         self.visit(node, node.block)
+        self.newline()
+        self.append('}')
+        self.newline()
+
+    def visit_RepeatUntil(self, parent, node):
+        self.append('do {')
+        self.newline()
+        self.visit(node, node.block)
+        self.newline()
+        self.append('}')
+        self.append('while (')
+        self.visit(node, node.cond)
+        self.append(');')
+        self.newline()
+
+    def visit_For(self, parent, node):
+        self.newline()
+        self.visit(node, node.init)
+        self.append(';')
+        self.newline()
+        self.indent()
+        self.append('while (')
+        self.visit(node, node.cond)
+        self.append(') {')
+        self.newline()
+        self.visit(node, node.block)
+        self.newline()
         self.level += 1
         self.indent()
         self.visit(node, node.step)
+        self.append(';')
+        self.newline()
+        self.append('}')
         self.level -= 1
+        self.newline()
 
     def visit_FuncImpl(self, parent, node):
-        self.append('def ')
+        self.visit(node, node.type_)
         self.append(node.id_.value)
         self.append('(')
         self.visit(node, node.params)
-        self.append('):')
+        self.append(') {')
         self.newline()
         self.visit(node, node.block)
+        self.newline()
+        self.append('}')
+        self.newline()
 
     def visit_FuncCall(self, parent, node):
         func = node.id_.value
@@ -180,14 +237,18 @@ class Generator(Visitor):
             self.visit(node, node.args)
             self.append(')')
 
+    def visit_ProcImpl(self, parent, node):
+        pass  # TODO
+
+    def visit_ProcCall(self, parent, node):
+        pass  # TODO
+
     def visit_Block(self, parent, node):
         self.level += 1
         for n in node.nodes:
-            self.append('{')
             self.indent()
             self.visit(node, n)
             self.newline()
-            self.append('}')
         self.level -= 1
 
     def visit_Params(self, parent, node):
@@ -196,11 +257,17 @@ class Generator(Visitor):
                 self.append(', ')
             self.visit(p, p.id_)
 
+    def visit_LocalVars(self, parent, node):
+        pass  # TODO
+
     def visit_Args(self, parent, node):
         for i, a in enumerate(node.args):
             if i > 0:
                 self.append(', ')
             self.visit(node, a)
+
+    def visit_WriteArg(self, parent, node):
+        pass  # TODO
 
     def visit_Elems(self, parent, node):
         for i, e in enumerate(node.elems):
@@ -215,6 +282,9 @@ class Generator(Visitor):
         self.append('continue;')
 
     def visit_Return(self, parent, node):
+        pass
+
+    def visit_Exit(self, parent, node):
         self.append('return')
         if node.expr is not None:
             self.append(' ')
@@ -227,9 +297,12 @@ class Generator(Visitor):
         elif node.value == 'string' or node.value == 'char':
             self.append('char ')
         elif node.value == 'real':
-            self.append('double ')
+            self.append('float ')
 
     def visit_Int(self, parent, node):
+        self.append(node.value)
+
+    def visit_Real(self, parent, node):
         self.append(node.value)
 
     def visit_Char(self, parent, node):
@@ -237,6 +310,12 @@ class Generator(Visitor):
 
     def visit_String(self, parent, node):
         self.append(node.value)
+
+    def visit_Bool(self, parent, node):
+        if node.value == 'true':
+            self.append(1)
+        elif node.vale == 'false':
+            self.append(0)
 
     def visit_Id(self, parent, node):
         self.append(node.value)
@@ -261,6 +340,9 @@ class Generator(Visitor):
         elif node.symbol != '&':
             self.append(node.symbol)
         self.visit(node, node.first)
+
+    def visit_NoneType(self, parent, node):
+        pass  # TODO
 
     def generate(self, path):
         self.visit(None, self.ast)
--- app/Test.py
+++ app/Test.py
@@ -3,13 +3,16 @@ from os import getcwd
 from IPython.core.display import Image
 
 import os
+
+from app.Symbolizer import Symbolizer
+
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 from app.Grapher import Grapher
 from app.Lexer import Lexer
 from app.Parser import Parser
 
-test_id = 1
+test_id = 12
 # path = f'{getcwd()}/data/pas/test{test_id}.pas'
 path = f'{getcwd()}/data/pas2/{test_id}/src.pas'
 
@@ -22,6 +25,9 @@ with open(path, 'r') as source:
     parser = Parser(tokens)
     ast = parser.parse()
     print(ast)
+
+    symbolizer = Symbolizer(ast)
+    symbolizer.symbolize()
 
     grapher = Grapher(ast)
     img = grapher.graph()
